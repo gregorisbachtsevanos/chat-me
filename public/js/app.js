fcce985ef79c033ae8554371dc2a24a4f52c4f8e
GregorisB <gregorbachts@gmail.com>
2022-05-28 23:10:06 +0300

    emir error

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,14 @@
 const socket = io()
-const form = document.querySelector('#form')
-const input = document.querySelector('#input')
-const messages = document.querySelector('#messages')
-const messageTemplate = document.querySelector('#messageTemplate')
+// const form = document.querySelector('#form')
+// const input = document.querySelector('#input')
+// const messages = document.querySelector('#messages')
+// const messageTemplate = document.querySelector('#messageTemplate')
 
-socket.on("connect", () => {
-    console.log(socket.id);
-});
-socket.on('message', msg => {
-    console.log(msg)
-    // const html = Mustache.render(messageTemplate, { msg })
-    // messages.insertAdjacentHTML('beforeend', html)
-})
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let msg = input.value
-    input.value = ''
-    socket.emit('sendMsg', msg)
-})
+    socket.on("connect", () => {
+        socket.emit('user_connect', {user, socketId:socket.id})
+    });
+    
+    socket.on('msg', (msg) =>{
+        console.log(msg)
+    })
